bace62d08fd1d8ea69ba0a2e7db704383e705bcb
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-03-14 17:11:08 -0300

    Insert users

--- src/main/java/com/leoabrantes/workshopmongo/services/UserService.java
+++ src/main/java/com/leoabrantes/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leoabrantes.workshopmongo.domain.User;
+import com.leoabrantes.workshopmongo.dto.UserDTO;
 import com.leoabrantes.workshopmongo.repository.UserRepository;
 import com.leoabrantes.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		
+		return repo.insert(obj);
+		
+	}
+	
+	public User fromDTO(UserDTO objDto){
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
