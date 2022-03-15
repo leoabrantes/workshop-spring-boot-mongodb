a514242de285aac4d0ea3da43b8f0dad34f749f6
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-03-15 16:19:25 -0300

    Simple Query with @Query

--- src/main/java/com/leoabrantes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leoabrantes/workshopmongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.leoabrantes.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.leoabrantes.workshopmongo.domain.Post;
@@ -11,6 +12,12 @@ import com.leoabrantes.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	
+ 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 
